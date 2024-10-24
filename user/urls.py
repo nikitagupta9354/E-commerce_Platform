ba0220383ac079c115860fbb5d1366fa37f1f741
Nikita Gupta <nikitagupta9354@gmail.com>
2024-10-24 15:18:32 +0530

    user module created

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import Registration,Login,Profile,ChangePassword,ResetPasswordEmail,ResetPassword
+   
+
+urlpatterns = [
+    path('register/',Registration.as_view(),name='register' ),
+    path('login/',Login.as_view(),name='login' ),
+    path('profile/',Profile.as_view(),name='profile' ),
+    path('changepassword/',ChangePassword.as_view(),name='change-password' ),
+    path('resetpasswordemail/',ResetPasswordEmail.as_view(),name='reset-password-email'),
+    path('resetpassword/<uid>/<token>/',ResetPassword.as_view(),name='reset-password')
+]
